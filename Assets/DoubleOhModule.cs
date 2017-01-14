61b20485d136da17189aea4e9ed033772318f69d
Timwi <github@timwi.de>
2017-01-14 19:34:45 +0100

    Unfinished

---
+++ Assets/DoubleOhModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DoubleOh;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Double-Oh
+/// Created by Timwi
+/// </summary>
+public class DoubleOhModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    void Start()
+    {
+        Debug.Log("[Double-Oh] Started");
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[Double-Oh] Activated");
+    }
+}
